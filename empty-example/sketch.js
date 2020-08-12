02880ef5947e976c82df020980cc2a4a49b7e98c
Stu Bolderson <stu.bolderson@aol.com>
2020-08-12 17:08:40 +0200

    base functionality added - work on reset ball, UI message, ball reset, color schemes and sound effects

--- empty-example/sketch.js
+++ empty-example/sketch.js
@@ -2,18 +2,83 @@
 
 "use strict";
 
-let ballRadius = 30;
-let ballPositionX = 200;
-let ballPositionY = 300;
+// Random Ball Placement
+var xBall = Math.floor(Math.random() * 300) + 50;
+var yBall = 50;
+var xSpeed = (2, 7);
+var ySpeed = (-7, -2);
+var score = 0
 
+// Canvas
 function setup() {
-  createCanvas(windowWidth, windowHeight);
+  createCanvas(400, 400);
 }
 
-function draw() {
-  background(2, 0, 36);
-  fill(255);
-  noStroke();
-  ellipse(ballPositionX, ballPositionY, ballRadius, ballRadius);
+//Background
 
+function draw() {
+
+  // Background
+  background(0);
+
+  // Paddle
+  fill('#ffffff');
+  rect(mouseX, 375, 90, 15);
+
+  //Functions
+  move();
+  display();
+  bounce();
+  //resetBall();
+  paddle();
+
+  //Score
+  fill('#d9c3f7');
+  textSize(24);
+  text("Score: " + score, 10, 25);
+}
+// Ball Functions
+function move() {
+  xBall += xSpeed;
+  yBall += ySpeed;
+}
+
+
+function bounce() {
+
+  if (xBall < 10 ||
+    xBall > 400 - 10) {
+    xSpeed *= -1;
+  }
+  if (yBall < 10 ||
+    yBall > 400 - 10) {
+    ySpeed *= -1;
+  }
+}
+
+
+// Reset Ball
+//function resetBall() {
+//  if (yBall >= 400 ||
+//    yBall > 400 - 10) {
+//    ySpeed = 4;
+// }
+
+//}
+
+function display() {
+  fill('#d9c3f7');
+  ellipse(xBall, yBall, 20, 20);
+}
+
+// Bounce off Paddle
+function paddle() {
+  if ((xBall > mouseX &&
+    xBall < mouseX + 90) &&
+    (yBall + 10 >= 375)) {
+    xSpeed *= -1;
+    ySpeed *= -1;
+    score++;
+
+  }
 }
